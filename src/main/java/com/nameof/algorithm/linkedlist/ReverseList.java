2ff91323d943919c1ca2ee63a761241fd3bf0aef
nameof <constexpr@foxmail.com>
2021-06-10 16:25:26 +0800

    reverse list

---
+++ src/main/java/com/nameof/algorithm/linkedlist/ReverseList.java
@@ -0,0 +1,13 @@
+package com.nameof.algorithm.linkedlist;
+
+public class ReverseList {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
